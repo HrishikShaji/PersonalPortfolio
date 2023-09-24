eb2d9275078145629127706a516930b244fac8e4
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-24 16:58:00 +0530

    Hero done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,49 +1,54 @@
 'use client'
+import gsap from 'gsap'
 import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react'
 
 export const Hero = () => {
-  const getBlocks = () => {
-    const blockSize = window.innerWidth * 0.05
-    const noOfBlocks = Math.ceil(window.innerHeight / blockSize)
-    console.log(noOfBlocks, 'running')
-    return [...Array(noOfBlocks).keys()].map((_, index) => {
-      return (
-        <div
-          className='w-full h-[5vw]'
-          onMouseEnter={(e) => colorize(e)}
-          key={index}
-        />
-      )
-    })
+  const word = 'HRISHIK SHAJI'
+  const splitWord = () => {
+    return word.split('').map((letter, i) => (
+      <div
+        className='text-animate translate-y-[220px] transition transform duration-700'
+        key={i}
+      >
+        {letter}
+      </div>
+    ))
   }
 
-  const colorize = (e) => {
-    e.target.style.backgroundColor = 'white'
-    setTimeout(() => {
-      e.target.style.backgroundColor = 'transparent'
-    }, 300)
-  }
+  useEffect(() => {
+    gsap.to('.text-animate', {
+      y: 0,
+      stagger: 0.1,
+      duration: 0.1,
+      ease: 'bounce'
+    })
+    const staggers = document.querySelectorAll('h2')
+    gsap.from(staggers, {
+      opacity: 0,
+      y: 20,
+      delay: 1,
+      ease: 'powerOne',
+      duration: 0.7,
+      stagger: 0.3
+    })
+  }, [])
+
   return (
-    <div className='h-screen w-full bg-neutral-900  overflow-hidden gap-10 flex flex-col justify-center items-center'>
-      <div className='z-10 flex flex-col relative mix-blend-difference pointer-events-none'>
-        <h1 className='font-bold  text-3xl'>I'M HRISHIK</h1>
+    <div className='h-screen w-full bg-neutral-900 flex flex-col justify-center items-center'>
+      <h1 id='text' className='custom-clip  text-[300px] flex'>
+        {splitWord()}
+      </h1>
+      <div>
         <h2>I'M A WEB DEVELOPER</h2>
-        <h1 className='text-teal-500'></h1>
-        <Link
-          href='https://github.com/HrishikShaji'
-          className='z-50 cursor-pointer pointer-events-auto'
-        >
-          GITHUB
-        </Link>
-      </div>
-      <div className='absolute w-full h-screen overflow-hidden flex'>
-        {[...Array(20).keys()].map((_, index) => {
-          return (
-            <div className='w-full h-full' key={index}>
-              {getBlocks()}
-            </div>
-          )
-        })}
+        <h2>
+          <Link
+            href='https://github.com/HrishikShaji'
+            className='z-50 cursor-pointer pointer-events-auto'
+          >
+            GITHUB
+          </Link>
+        </h2>
       </div>
     </div>
   )
