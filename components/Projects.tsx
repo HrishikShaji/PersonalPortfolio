b58f0ce433603ac3413a9a6dd7fdfc4383d9d08e
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-25 21:24:48 +0530

    projects started

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,30 @@
+'use client'
+
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+import { useEffect } from 'react'
+
 export const Projects = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const projectsTitle = document.querySelector('.projects-title')
+    const projectsParent = document.querySelector('.projects-parent')
+    gsap.from(projectsTitle, {
+      scale: 5,
+      scrollTrigger: {
+        trigger: projectsParent,
+        start: 'top 70%',
+        markers: true,
+        scrub: true
+      }
+    })
+  }, [])
   return (
-    <div className='h-screen w-full bg-neutral-900 flex flex-col p-10'>
-      <h1 className='text-white font-bold text-3xl'>MY PROJECTS</h1>
+    <div className='projects-parent h-screen w-full bg-neutral-900 justify-center flex flex-col items-center p-10 overflow-hidden'>
+      <h1 className='projects-title text-white leading-none text-[150px]'>
+        MY PROJECTS
+      </h1>
     </div>
   )
 }
