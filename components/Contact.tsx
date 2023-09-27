8c13c768c3331dc47ff1e42015ef57e5a1db9344
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-27 16:33:39 +0530

    contact done

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,31 +8,66 @@ export const Contact = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
 
-    const inputs = document.querySelectorAll('.inputs')
     const contactContainer = document.querySelector('.contact-container')
-    gsap.from(inputs, {
-      scaleY: 0,
-      scrollTrigger: {
-        trigger: contactContainer,
-        start: 'top 99%'
-      }
+    const inputContainer = document.querySelectorAll('.input-container')
+    inputContainer.forEach((input) => {
+      const line = input.querySelector('div')
+      const inputText = input.querySelector('.inputs')
+
+      gsap.fromTo(
+        inputText,
+        { opacity: 0 },
+        {
+          opacity: 1,
+          scrollTrigger: {
+            trigger: input,
+            start: 'top 70%',
+            end: 'bottom 75%',
+            markers: true,
+            scrub: 1
+          }
+        }
+      )
+      gsap.fromTo(
+        line,
+        { scaleX: 0 },
+        {
+          scaleX: 1,
+          scrollTrigger: {
+            trigger: input,
+            start: 'top 70%',
+            end: 'bottom 75%',
+            markers: true,
+            scrub: 3
+          }
+        }
+      )
     })
   }, [])
   return (
-    <div className='contact-container h-full w-full bg-neutral-900 p-10 flex flex-col'>
+    <div className='contact-container h-full w-full bg-neutral-900 p-10 flex pb-40 flex-col'>
       <form className='flex flex-col w-[70%] gap-2'>
-        <input
-          className='inputs bg-transparent p-2 border-b-2 border-white'
-          placeholder='Name...'
-        />
-        <input
-          className='inputs p-2 border-b-2 bg-transparent border-white'
-          placeholder='Email...'
-        />
-        <textarea
-          className='inputs h-[100px] p-2 border-b-2 bg-transparent border-white'
-          placeholder='Message...'
-        />
+        <div className='input-container'>
+          <input
+            className='inputs bg-transparent origin-left  p-2 w-full focus:outline-none'
+            placeholder='Name...'
+          />
+          <div className='h-1 w-full bg-white origin-left' />
+        </div>
+        <div className='input-container'>
+          <input
+            className='inputs p-2 origin-left  bg-transparent w-full focus:outline-none'
+            placeholder='Email...'
+          />
+          <div className='h-1 w-full bg-white origin-left' />
+        </div>
+        <div className='input-container'>
+          <textarea
+            className='inputs h-[100px] p-2 origin-left bg-transparent w-full focus:outline-none'
+            placeholder='Message...'
+          />
+          <div className='h-1 w-full bg-white origin-left' />
+        </div>
         <button className='p-2 bg-white text-black text-left'>SEND</button>
       </form>
     </div>
